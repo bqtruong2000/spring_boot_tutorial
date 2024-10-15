8ca0443288d60f1cd55a63261542059f1c93f039
bqtruong2000 <bqtruong2021@gmail.com>
2024-10-15 14:52:54 +0700

    JpQL - Search

--- src/main/java/com/spring_boot/spring_boot_tutorial/repository/ClubRepository.java
+++ src/main/java/com/spring_boot/spring_boot_tutorial/repository/ClubRepository.java
@@ -2,9 +2,13 @@ package com.spring_boot.spring_boot_tutorial.repository;
 
 import com.spring_boot.spring_boot_tutorial.models.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClubRepository extends JpaRepository<Club, Long> {
     Optional<Club> findByTitle(String title);
+    @Query("SELECT c from Club c WHERE c.title LIKE CONCAT('%', :query, '%')")
+    List<Club> searchClubs(String query);
 }
