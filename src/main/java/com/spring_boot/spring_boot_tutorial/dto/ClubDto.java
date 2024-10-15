45e12b3b8a0b2f56f7147a12fdd55ca6b51a82e6
bqtruong2000 <bqtruong2021@gmail.com>
2024-10-15 10:54:21 +0700

    CRUD-Update

--- src/main/java/com/spring_boot/spring_boot_tutorial/dto/ClubDto.java
+++ src/main/java/com/spring_boot/spring_boot_tutorial/dto/ClubDto.java
@@ -3,6 +3,7 @@ package com.spring_boot.spring_boot_tutorial.dto;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
@@ -13,8 +14,11 @@ import java.time.LocalDateTime;
 @Builder
 public class ClubDto {
     private Long id;
+    @NotEmpty(message = "Club title should not be empty")
     private String title;
+    @NotEmpty(message = "Club photo URL should not be empty")
     private String photoUrl;
+    @NotEmpty(message = "Club content should not be empty")
     private String content;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
--- src/main/java/com/spring_boot/spring_boot_tutorial/service/ClubService.java
+++ src/main/java/com/spring_boot/spring_boot_tutorial/service/ClubService.java
@@ -1,9 +1,16 @@
 package com.spring_boot.spring_boot_tutorial.service;
 
 import com.spring_boot.spring_boot_tutorial.dto.ClubDto;
+import com.spring_boot.spring_boot_tutorial.models.Club;
 
 import java.util.List;
 
 public interface ClubService {
     List<ClubDto> findAllClubs();
+
+    Club saveClub(Club club);
+
+    ClubDto findClubById(Long clubId);
+
+    void updateClub(ClubDto club);
 }
